7ed7e14a780b58885864301b465e05eece40cdf5
Alfonsina <alfonsina.l@gmail.com>
2020-10-27 20:29:36 -0300

    feat: api call working properly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Formulario from './components/Formulario'
+import {API_KEY} from './API_KEY'
 
 function App() {
+  const [busqueda, guardarBusqueda] = useState('')
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      if (!busqueda) return
+      
+      const imagenesPorPagina = 30;
+      const url = `https://pixabay.com/api/?key=${API_KEY}&q=${busqueda}&per_page=${imagenesPorPagina}`
+  
+      const respuesta = await fetch(url)
+      const resultado = await respuesta.json()
+      guardarBusqueda(resultado.hits)
+    }
+    consultarApi();
+  }, [busqueda])
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de Imágenes</p>
-        <Formulario />
+        <Formulario
+          guardarBusqueda={guardarBusqueda}
+        />
       </div>
     </div>
   );
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Error from './Error'
 
-const Formulario = () => {
+const Formulario = ({ guardarBusqueda }) => {
+  const [termino, guardarTermino] = useState('')
+  const [error, guardarError] = useState(false)
+
+  const buscarImagenes = e => {
+    e.preventDefault()
+    
+    //validar
+    if (termino.trim() === '') {
+      guardarError(true)
+      return
+    }
+    guardarError(false)
+    guardarBusqueda(termino)
+
+    //enviar la busqueda al componente principal
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={buscarImagenes}
+    >
       <div className="row">
         <div className="form-group col-md-8">
           <input
             type="text"
             className="form-control form-control-lg"
             placeholder="Busca una imagen, ejemplo: futbol o café"
+            value={termino}
+            onChange={e => guardarTermino(e.target.value)}
           />
         </div>
         <div className="form-group col-md-4">
@@ -19,6 +41,7 @@ const Formulario = () => {
           />
         </div>
       </div>
+      {error && <Error mensaje="Agrega un término de búsqueda" />}
     </form>
   )
 }
