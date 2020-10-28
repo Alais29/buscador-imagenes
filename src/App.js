d48d6f5d9be21df3836f965990ec2872b78b4738
Alfonsina <alfonsina.l@gmail.com>
2020-10-27 21:24:35 -0300

    feat: Added components to show images, and buttons for next and previous pagination

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import Formulario from './components/Formulario'
 import {API_KEY} from './API_KEY'
+import Listadoimagenes from './components/ListadoImagenes'
 
 function App() {
   const [busqueda, guardarBusqueda] = useState('')
+  const [imagenes, guardarImagenes] = useState([])
+  const [paginaActual, guardarPaginaActual] = useState(1)
+  const [totalPaginas, guardarTotalPaginas] = useState(1)
 
   useEffect(() => {
     const consultarApi = async () => {
       if (!busqueda) return
       
       const imagenesPorPagina = 30;
-      const url = `https://pixabay.com/api/?key=${API_KEY}&q=${busqueda}&per_page=${imagenesPorPagina}`
+      const url = `https://pixabay.com/api/?key=${API_KEY}&q=${busqueda}&per_page=${imagenesPorPagina}&page=${paginaActual}`
   
       const respuesta = await fetch(url)
       const resultado = await respuesta.json()
-      guardarBusqueda(resultado.hits)
+      guardarImagenes(resultado.hits)
+      guardarTotalPaginas(Math.ceil(resultado.totalHits / imagenesPorPagina))
+
+      //Mover la pantalla hacia arriba
+      const jumbotron = document.querySelector('.jumbotron')
+      jumbotron.scrollIntoView({behavior: 'smooth'})
     }
     consultarApi();
-  }, [busqueda])
+  }, [busqueda, paginaActual])
+
+  const paginaAnterior = () => {
+    const nuevaPaginaActual = paginaActual - 1
+    if(nuevaPaginaActual === 0) return
+    guardarPaginaActual(nuevaPaginaActual)
+  }
+
+  const paginaSiguiente = () => {
+    const nuevaPaginaActual = paginaActual + 1
+    if (nuevaPaginaActual > totalPaginas) return
+    guardarPaginaActual(nuevaPaginaActual)
+  }
 
   return (
     <div className="container">
@@ -26,6 +47,29 @@ function App() {
         <Formulario
           guardarBusqueda={guardarBusqueda}
         />
+      </div>
+      <div className="row justify-content-center">
+        <Listadoimagenes
+          imagenes={imagenes}
+        />
+        {paginaActual !== 1 &&
+          <button
+            type="button"
+            className="btn btn-info mr-1"
+            onClick={paginaAnterior}
+          >
+            Anterior
+          </button>
+        }
+        {paginaActual !== totalPaginas &&
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={paginaSiguiente}
+          >
+            Siguiente
+          </button>
+        }
       </div>
     </div>
   );
